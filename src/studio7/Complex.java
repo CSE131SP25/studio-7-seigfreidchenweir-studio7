8c18b2ebdd2f0914c739dad4ed1e4059069cad10
haileyweir <haileyweir@localhost>
2025-03-26 17:19:13 -0500

    part 2

--- src/studio7/Complex.java
+++ src/studio7/Complex.java
@@ -1,6 +1,25 @@
 package studio7;
 
 public class Complex {
+	
+	private double real;
+	private double imaginary;
+	
+	public Complex(double r, double i) {
+		this.real = r;
+		this.imaginary = i;
+	}
+	
+	private String realI() {
+		return "" + real + "+" + imaginary + "i";
+	}
+	
+	private String I()
+	
+	
+	
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -1,10 +1,41 @@
 package studio7;
 
 public class Fraction {
+private double denom;
+private double num;
 
+public Fraction (double denom, double num) {
+	this.denom = denom;
+	this.num = num;
+}
+private void Multiply (Fraction a) {
+	denom = this.denom * a.denom;
+	num = this.num * a.num;
+}
+
+private void add (Fraction a) {
+	if (this.denom == a.denom) {
+		denom = this.denom;
+		num = this.num * a.num;
+	}
+	else {
+		denom = this.denom * a.denom;
+		num = (this.num*a.denom) + (a.num*this.denom);
+	}
+}
+
+private void reciprical() {
+	denom = this.num;
+	num = this.denom;
+	}
+
+private void simplify() {
+	
+}
+ 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
 }
+
